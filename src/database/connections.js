e160994c4e30f61b504f92a7560c6d7d988c76a7
LUCAS CARNEIRO <lucas.carneiro098@gmail.com>
2021-09-13 22:56:18 -0300

    package json

---
+++ src/database/connections.js
@@ -0,0 +1,31 @@
+const {MongoClient, ObjectId} = require("mongodb");
+const dotenv = require("dotenv").config();
+
+const connectionString = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.${process.env.DB_CHAR}.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`;
+
+const client = new MongoClient(connectionString, {
+  useUnifiedTopology: true,
+  useNewUrlParser: true,
+});
+const db = client.db(process.env.DB_NAME);
+const rooms = db.collection("rooms");
+
+async function connect() {
+    try{
+  await client.connect(() => {
+    console.log("Connection established");
+  })} catch(err) {
+      await client.connect(() => {
+        console.log("Connection forcelly established");
+    })};
+};
+
+async function dbclose() {
+  try {
+    await client.close(false, () => console.log("Conection closed"));
+  } catch (err) {
+    await client.close(true, () => console.log("Conection forcelly closed"));
+  }
+}
+
+module.exports = {client, dbclose, ObjectId, db, rooms, connect};
